05232b327e097f4e273d0d7a3b4d7eef16596b24
joshuazg <86587114+joshuazg@users.noreply.github.com>
2023-09-14 02:33:30 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -26,35 +26,8 @@ db_conn = connections.Connection(
 )
 output = {}
 
-@app.route("/", methods=['GET', 'POST'])
-def home():
-    return render_template('publishIntern.html')
 
-@app.route("/addInternFormCom", methods=['POST'])
-def AddInternFormCom():
-
-    job_title = request.form['job_title']
-    job_desc = request.form['job_description']
-    job_salary = request.form['job_salary']
-    job_location = request.form['job_location']
-    workingDay = request.form['workingDay']
-    workingHour = request.form['workingHour']
-    accomodation = request.form['accomodation']
-
-    print("%s %s %s %s %s %s %s", job_title, job_desc, job_salary, job_location,
-          workingDay, workingHour,accomodation)
-
-    #insert_sql = "INSERT INTO student VALUES (%s, %s)"
-    #cursor = db_conn.cursor()
-
-    #cursor.close()
-
-    #print("all modification done...")
-    #return render_template('AddStudOutput.html', name=stud_name)
-
-
-
-@app.route("/searchstud", methods=['GET'])
+@app.route("/displayIntern", methods=['GET'])
 def get_intern_com_details():
     try:
         # Corrected SQL statement with placeholder
@@ -75,39 +48,6 @@ def get_intern_com_details():
         
     finally:
         cursor.close()
-
-
-@app.route("/updatestud", methods=['POST'])
-def UpStud():
-    try:
-        stud = request.form['update_id']
-        name = request.form['update_name']
-
-        # Corrected SQL statement with placeholder
-        statement_get = "SELECT stud_name FROM student WHERE stud_id = %s"
-        cursor = db_conn.cursor()
-        cursor.execute(statement_get, (stud,))
-        
-        # Fetch the result
-        result = cursor.fetchone()
-
-        if result:
-
-            stud_name = result[0]
-            statement = "UPDATE student SET stud_name = %s WHERE stud_id = %s"
-            cursor.execute(statement, (name, stud))
-          
-            return render_template('updateStudent.html', new=name, id=stud, old=stud_name)
-        else:
-            return render_template('updateError.html', id=stud)
-        
-    except Exception as e:
-        return str(e)
-        
-    finally:
-        cursor.close()
-
-
         
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
