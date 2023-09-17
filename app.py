5041df3f107fdfbf6bb6062ce482918d4906ce4a
joshuazg <86587114+joshuazg@users.noreply.github.com>
2023-09-17 17:25:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -68,10 +68,9 @@ def jobList(cate):
     cursor = db_conn.cursor()
     cursor.execute(cate_statement, (cate))
     list = cursor.fetchall()
-    typ = cursor.fetchone()
     cursor.close()
     
-    return render_template('job_listing.html', listing = list, type = typ)
+    return render_template('job_listing.html', listing = list)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
