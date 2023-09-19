c7946f0a7e7ace346aabf590e7b69f1d244ed74a
joshuazg <86587114+joshuazg@users.noreply.github.com>
2023-09-20 02:29:19 +0800

    Update test.py

--- test/test.py
+++ test/test.py
@@ -40,7 +40,14 @@ def search():
     results = cursor.fetchall()
     cursor.close()
 
-    return render_template('search_result.html', results=results)
+    search_statement = """SELECT i.intern_id, c.com_name, i.job_title, i.intern_salary, i.location, i.workingDay, i.workingHour, c.industry_involve 
+    FROM Internship i INNER JOIN Company c WHERE i.com_id = c.com_id"""
+    cursor = db_conn.cursor()
+    cursor.execute(search_statement)
+    search = cursor.fetchall()
+    cursor.close()
+
+    return render_template('search_results.html', results=results, search=search)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
